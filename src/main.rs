d0c3030c7991605854dcf15b2c6296bf5e57ddbc
Anosh Damania <anoshphiroze.damania2020@vitstudent.ac.in>
2026-05-18 12:53:54 +0530

    Initial: set up egui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 fn main() {
-    println!("Hello, world!");
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "My egui App",
+        options,
+        Box::new(|_cc| {
+
+            Box::new(MyEguiApp::default())
+        }),
+    ).unwrap();
+}
+
+#[derive(Default)]
+struct MyEguiApp {}
+
+impl eframe::App for MyEguiApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello, egui!");
+        });
+    }
 }
